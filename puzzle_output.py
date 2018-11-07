6fcb00304ba3c38a272c0c122e6491b221ee1978
ParthThakur <parththakur@gmail.com>
2018-11-08 02:16:45 +0530

    remove redundant lines of code

--- puzzle_output.py
+++ puzzle_output.py
@@ -1,9 +1,7 @@
 import pandas as pd
-import numpy as np
 from Solution import solve
 
 puzzle_input = pd.read_csv("puzzle_input.csv", header=None)
-# puzzle_input.replace('X', 0, inplace=True)
 
 print(puzzle_input, end="\n\n")
 grid_size = int(puzzle_input[0][0])
