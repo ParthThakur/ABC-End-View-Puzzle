45c5e9652dadab4fa80ba3e8c400f3cfc7405235
ParthThakur <parththakur@gmail.com>
2018-11-16 20:27:57 +0530

    Change output file naming conventions

--- PuzzleSolver.py
+++ PuzzleSolver.py
@@ -7,7 +7,10 @@ __email__ = "parththakur@gmail.com"
 import pandas as pd
 from DFS import solve
 
-puzzle_input = pd.read_csv("test puzzles/puzzle_1.csv", header=None)
+
+puzzle_no = "12"
+
+puzzle_input = pd.read_csv("test puzzles/puzzle_"+puzzle_no+".csv", header=None)
 puzzle_input.replace('X', 0, inplace=True)
 
 print(puzzle_input, end="\n\n")
@@ -22,12 +25,13 @@ right = puzzle_input.T[5].tolist()
 solution = solve(grid_size, letter_set, top, bottom, left, right)
 
 if solution[0]:
-    name = "puzzle_output.csv"
+    name = "puzzle_output_"
 else:
-    name = "puzzle_output_(bestCase).csv"
+    name = "puzzle_output_(bestCase)_"
+
+output_path = "test_output\\"+name+puzzle_no+".csv"
 
 df = pd.DataFrame(solution[1].board_current_state())
-df.replace(' ', 'X').to_csv(name, header=False, index=False)
+df.replace(' ', 'X').to_csv(output_path, header=False, index=False)
 
-print("The solution was saved in puzzle_output.csv")
-input("Press any key to exit.")
+print("The solution was saved in", output_path)
