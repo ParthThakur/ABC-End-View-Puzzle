efb928783dc6cf3416a8354dec5dd0916dce81c3
ParthThakur <parththakur@gmail.com>
2019-01-25 01:43:24 +0530

    try efficiency improvements

--- Board.py
+++ Board.py
@@ -9,9 +9,27 @@ def iscorner(row, column, g_size):
                 all([row == g_size, column == 0])])
 
 
-def get_central_array(array, num_blanks):
+def central_array(array, num_blanks):
     x, y = array.shape
-    return array[num_blanks:x - num_blanks, num_blanks:y-num_blanks]
+    for index, value in np.ndenumerate(array[num_blanks:x - num_blanks, num_blanks:y-num_blanks]):
+        yield index, value
+
+
+def corners(array):
+    for index, value in np.ndenumerate(array[::array.shape[0]-1, ::array.shape[1]-1]):
+        yield index, value
+
+
+def sides(array, num_blank):
+    x, y = array.shape
+    iter1 = np.ndenumerate(array[[*list(range(num_blank+1)), *list(range(x-num_blank-1, x))]])
+    iter2 = np.ndenumerate(array[:, [*list(range(num_blank)), *list(range(x - num_blank-1, x))]])
+    for iter_ in [iter1, iter2]:
+        for index, value in iter_:
+            if iscorner(*index, x):
+                continue
+            else:
+                yield index, value
 
 
 class Board:
@@ -30,8 +48,9 @@ class Board:
     def __repr__(self):
         return pd.DataFrame(self.board).to_string()
 
-    def freeze_cells(self):
-        for cell in self.board.flat:
+    @classmethod
+    def freeze_cells(cls):
+        for cell in cls.board.flat:
             cell.freeze_values()
 
     @classmethod
@@ -60,29 +79,27 @@ class Board:
         top_bot = [top_constraint, bot_constraints]
         left_right = [left_constraints, right_constraints]
 
-        for (row, column), cell in np.ndenumerate(cls.board):
-            if iscorner(row, column, cls.grid_size):
-                c_1 = top_bot[0 if column == 0 else 1][0 if row == 0 else cls.grid_size]
-                c_2 = left_right[0 if row == 0 else 1][0 if column == 0 else cls.grid_size]
+        for (row, column), cell in corners(cls.board):
+            c_1 = top_bot[0 if column == 0 else 1][0 if row == 0 else cls.grid_size]
+            c_2 = left_right[0 if row == 0 else 1][0 if column == 0 else cls.grid_size]
 
-                if c_1 == c_2:
-                    cell.value_set = {c_1}
-                else:
-                    cell.value_set = {c_2, c_1}
-                print(row, column, c_1, c_2)
-
+            if c_1 == c_2:
+                cell.value_set = {c_1}
             else:
-                if row <= cls.num_blank:
-                    cell.value_set.add(top_constraint[column])
-                elif row >= cls.grid_size - cls.num_blank:
-                    cell.value_set.add(bot_constraints[column])
+                cell.value_set = {c_2, c_1}
+            print(row, column, c_1, c_2)
 
-                if column <= cls.num_blank:
-                    cell.value_set.add(left_constraints[row])
-                elif column >= cls.grid_size - cls.num_blank:
-                    cell.value_set.add(right_constraints[row])
+        for (row, column), cell in sides(cls.board, cls.num_blank):
+            if row <= cls.num_blank:
+                cell.value_set.add(top_constraint[column])
+            elif row >= cls.grid_size - cls.num_blank:
+                cell.value_set.add(bot_constraints[column])
 
-        cls.freeze_cells(cls)
+            if column <= cls.num_blank:
+                cell.value_set.add(left_constraints[row])
+            elif column >= cls.grid_size - cls.num_blank:
+                cell.value_set.add(right_constraints[row])
+        cls.freeze_cells()
 
 
 class Cell:
@@ -92,7 +109,7 @@ class Cell:
         self.value_set = set()
 
     def freeze_values(self):
-        self.value_set = {*self.value_set}.remove(np.nan)
+        self.value_set.discard(np.nan)
 
     def __repr__(self):
         return str(self.value_set)
