81bef5e92a7e3f1ac54a5069e7f24844837a6d95
Parth <parththakur@gmail.com>
2019-01-30 11:48:36 +0530

    Improve Initial state

--- BFS.py
+++ BFS.py
@@ -1,10 +1,12 @@
 from time import time
-from Board import Board
+import Board
 
 
 def solve(constraints, grid_size, letter_options=None):
     start = time()
-    board = Board(grid_size)
+    board = Board.Board(grid_size)
     board.set_initial_state(constraints, letter_options)
+    print(board)
+    Board.Cell.return_value_set = False
     print(board)
     print(time() - start)
--- Board.py
+++ Board.py
@@ -16,7 +16,7 @@ def corners(array):
 
 def sides(array):
     x, y = array.shape
-    corners = [(0, 0), (x-1, y-1), (0, x-1), (y-1,0)]
+    corners_ = [(0, 0), (x-1, y-1), (0, x-1), (y-1,0)]
     iter1 = array[0].flat
     iter2 = array[:, 0].flat
     iter3 = array[-1].flat
@@ -24,14 +24,14 @@ def sides(array):
 
     for iter_ in [iter1, iter2, iter3, iter4]:
         for value in iter_:
-            if value.position in corners:
+            if value.position in corners_:
                 continue
             yield value
 
 
 def remnants(array):
     for cell in array.flat:
-        if cell.isnan:
+        if cell.isnan_valueset:
             yield cell
 
 
@@ -56,6 +56,9 @@ class Board:
     def __repr__(self):
         return pd.DataFrame(self.board).to_string()
 
+    def __iter__(self):
+        return self.board.flat
+
     @classmethod
     def freeze_cells(cls):
         for cell in cls.board.flat:
@@ -63,8 +66,11 @@ class Board:
 
     @classmethod
     def add_nan(cls):
+        corner_cells = list(corners(cls.board))
         for cell in cls.board.flat:
-            cell.value_set = {' ', *cell.value_set}
+            cell.value_set = set(cell.value_set)
+            if cell not in corner_cells:
+                cell.value_set |= {' '}
 
     @classmethod
     def set_initial_state(cls, initial_states, letter_set=None):
@@ -108,11 +114,16 @@ class Board:
 
             if close_1 == close_2:
                 cell.value_set = {close_1}
-            elif close_1 != close_2:
-                cell.value_set = set()
+            else:
+                if [str(close_1), str(close_2)] == ['nan', 'nan']:
+                    cell.value_set = {' ', *[x for x in cls.letter_options if x not in [far_1, far_2]]}
+                elif str(close_1) == 'nan' or str(close_2) == 'nan':
+                    cell.value_set = {str(close_1), str(close_2), ' '}
+                else:
+                    cell.value_set = {' '}
 
             if [str(close_1), str(close_2)] == ['nan', 'nan']:
-                cell.value_set = {*[x for x in cls.letter_options if x not in [far_1, far_2]]}
+                cell.value_set = {' ', *[x for x in cls.letter_options if x not in [far_1, far_2]]}
 
             cell.freeze_values()
 
@@ -127,7 +138,7 @@ class Board:
             elif column == cls.grid_size:
                 cell.value_set = {right_constraints[row]}
 
-            if not cell.isnan:
+            if not cell.isnan_valueset:
                 cell.freeze_values()
 
         for cell in centre(cls.board, cls.num_blank):
@@ -155,115 +166,9 @@ class Board:
 
         cls.add_nan()
 
-    def check_cell(self, cell, letter):
-        """
-        Checks the value of the given cell.
-        :param cell: Cell object to be checked.
-        :param letter: Value of cell to check.
-        :return: True if OK, False if not.
-        """
-        board_fix_values = self.board_current_state()
-        (r, c) = cell.position
-        try_value = str(letter)
-        row = np.delete(board_fix_values[r], c)
-        column = np.delete(board_fix_values[:, c], r)
-
-        # Check if value is ' '.
-        # If ' ', return True only if the number of NaNs in row or column are
-        # acceptable.
-        if try_value == ' ':
-            if sum(row[:c] == ' ') < self.num_blank:
-                if sum(column[:r] == ' ') < self.num_blank:
-                    return True
-            else:
-                return False
-
-        # If letter already exists in row ar column, return False.
-        if try_value in row or try_value in column:
-            return False
-
-        # Check entire Row.
-        if 0 <= r <= self.grid_size:
-
-            # If option is equal to top constraint, return true only if ' '
-            # above of cell.
-            # Also check if row index is permissible. i.e Cell is not too far
-            # down, making other cells in row invalid.
-            if try_value == self.top[c]:
-                if r > self.num_blank:
-                    return False
-                if not (board_fix_values[:, c][:r] == ' ').all():
-                    return False
-            else:
-                if self.top[c] != 0:
-                    if set(column[:r]) == {' '}:
-                        return False
-
-            # If option is equal to bottom constraint, return true only if ' '
-            # below cell.
-            # Also check if row index is permissible. i.e Cell is not too far
-            # up, making other cells in row invalid.
-            if try_value == self.bot[c]:
-                if r < self.grid_size - self.num_blank - 1:
-                    return False
-                if not (board_fix_values[r + 1:][:, c] == ' ').all():
-                    return False
-            else:
-                if self.bot[c] in column.tolist():
-                    return False
-
-        # Check entire Column
-        if 0 <= c <= self.grid_size:
-
-            # If option is equal to left constraint, return true only if ' '
-            # on left of the cell.
-            # Also check if column index is permissible. i.e Cell is not too far
-            # right, making other cells in row invalid.
-            if try_value == self.left[r]:
-                if c > self.num_blank:
-                    return False
-                if not (board_fix_values[r][:c] == ' ').all():
-                    return False
-            else:
-                if self.left[r] != 0:
-                    if set(row[:c]) == {' '}:
-                        return False
-
-            # If option is equal to right constraint, return true only if ' '
-            # on right of the cell.
-            # Also check if column index is permissible. i.e Cell is not too far
-            # left, making other cells in row invalid.
-            if try_value == self.right[r]:
-                if c < self.grid_size - 1 - self.num_blank:
-                    return False
-                if not (board_fix_values[r][c + 1:] == ' ').all():
-                    return False
-            else:
-                if self.right[r] in row.tolist():
-                    return False
-        return True
-
-    def board_current_state(self):
-        """
-        :return: NumPy array of current values of individual cells.
-        """
-        set_values = []
-        for rows in self.board:
-            set_values.append([cell.value for cell in rows])
-        return np.array(set_values)
-
-    def remove_options(self, cell):
-        letter = cell.value
-        r, c = cell.position
-        row = self.board[r][c + 1:]
-        column = self.board[:, c][r + 1:]
-
-        for box in [*row, *column]:
-            box.remove_options(letter)
-        return None
-
 
 class Cell:
+    return_value_set = True
 
     def __init__(self, row, column):
         self.value = None
@@ -280,12 +185,15 @@ class Cell:
             pass
 
     @property
-    def isnan(self):
+    def isnan_valueset(self):
         check = [x for x in self.value_set if str(x) != 'nan']
         return not bool(check) and type(self.value_set) != frozenset
 
-    def __repr__(self):
-        return str(self.value)
+    @property
+    def isnan(self):
+        return str(self.value) in ['nan', ' ', '']
 
-    def __call__(self, option):
-        self.value = option
+    def __repr__(self):
+        if self.return_value_set:
+            return str(self.value_set)
+        return str(self.value)
